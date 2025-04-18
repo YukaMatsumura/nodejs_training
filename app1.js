0dfd0b1018fbb353f18b22d34670b2b7afe783b5
YukaMatsumura <2201130008jg@cyber-u.ac.jp>
2025-04-18 09:33:01 +0900

    app1.js
    コメントを追加

--- app1.js
+++ app1.js
@@ -1,8 +1,8 @@
-const http = require('http');
+const http = require('http');//サーバオブジェクトを作成
 
-var server = http.createServer(
+var server = http.createServer(//サーバにアクセスがあった時の動作を記述
   (request,response)=>{
     response.end('Hello Node.js!');
   }
 );
-server.listen(3000);
+server.listen(3000);//ポート3000で待ち受け状態にする
